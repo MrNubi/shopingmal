d9797295636894b97d3fef372acc47cbb35832de
[jeahunDeskTop] <[jeahun1@naver.com]>
2023-09-10 03:25:32 +0900

    12345

---
+++ frontend/src/store/index.js
@@ -0,0 +1,42 @@
+import {configureStore,  combineReducers, getDefaultMiddleware } from '@reduxjs/toolkit'
+import userReducer from './userSlice'
+import storage from 'redux-persist/lib/storage'
+import { FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE, persistReducer, persistStore } from 'redux-persist';
+
+
+//서로 다른 리듀싱 함수들을 값으로 가지는 객체를 받아서 createStore에 넘길 수 있는 하나의 리듀싱 함수로 반환
+export const rootReducer = combineReducers({
+    user: userReducer
+});
+
+const persistConfig = {
+    key: "root",//key이름
+    storage, // localStorage에 저장하겠단 뜻
+    // whitelist : []  // 여러 리듀서중에 해당 리듀서만 localStorage에 저장
+    // blackList : [] // 여러 리듀서중에 해당 리듀서만 제외하고 저장
+
+}
+
+
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({reducer:{
+    reducer: persistedReducer,
+    middleware: getDefaultMiddleware => getDefaultMiddleware({
+
+        serializableCheck: {
+            ignoredActions:[FLUSH,REHYDRATE,PAUSE,PERSIST,PURGE,REGISTER]   
+        }
+    
+    
+    }) 
+}
+
+})
+
+export const persistor = persistStore(store)
+
+//
+
+
