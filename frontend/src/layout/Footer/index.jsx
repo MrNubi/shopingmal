32e38e0d515e0f2a4dd53317acc4e024d478fb96
[jeahunDeskTop] <[jeahun1@naver.com]>
2023-09-10 02:00:10 +0900

    start commit

--- frontend/src/layout/Footer/index.jsx
+++ frontend/src/layout/Footer/index.jsx
@@ -2,8 +2,8 @@ import { AiOutlineSmile } from 'react-icons/ai'
 const Footer = () => {
   return (
     <div className='flex items-center justify-center h-20 text-lg'>
-      
-      <AiOutlineSmile/>All rights reserved<AiOutlineSmile/>
+       
+      <AiOutlineSmile/>All rights reserved<AiOutlineSmile/><AiOutlineSmile/>
     </div>
   )
 }
